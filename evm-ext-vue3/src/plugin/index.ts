559aab047e6556b219cd0a948bf44aca45f6e6f9
Gregory Rogachev <kotsmilewtm@gmail.com>
2022-12-10 17:59:19 +0300

    implement store functions for plugin

--- evm-ext-vue3/src/plugin/index.ts
+++ evm-ext-vue3/src/plugin/index.ts
@@ -1,3 +1,19 @@
-import { plugin } from 'evm-ext'
+import type { plugin } from 'evm-ext'
 
-export const vuePlugin: plugin.Plugin = () => ({})
+import { useEvent } from './stores/event'
+
+const stores = {
+  event: useEvent,
+}
+
+export const vuePlugin: plugin.Plugin = () => ({
+  getValue(n, k) {
+    return stores[n]()[k]
+  },
+  setValue(n, k, v) {
+    return (stores[n]()[k] = v)
+  },
+  updateValue(n, k, c) {
+    return (stores[n]()[k] = c(stores[n]()[k]))
+  },
+})
---
+++ evm-ext-vue3/src/plugin/stores/event.ts
@@ -0,0 +1,19 @@
+import { defineStore } from 'pinia'
+import type { modules } from 'evm-ext'
+
+type EventState = modules.events.Storage['event']
+
+export const useEvent = defineStore('$event', {
+  state: () =>
+    <EventState>{
+      listeners: [] as {
+        id: number
+        event: modules.events.EventType
+        once: boolean
+        callback: modules.events.CallbackFunction
+        filters: modules.events.Filter<any>[]
+      }[],
+      listenerId: 1,
+    },
+  actions: {},
+})
