b8feeceec30b8c03f740b562e2ff5cd30c058be4
Gregory Rogachev <kotsmilewtm@gmail.com>
2022-12-12 16:52:54 +0300

    add to main export safe functions

--- evm-ext/src/index.ts
+++ evm-ext/src/index.ts
@@ -3,4 +3,6 @@ export { defineEvmConfig }
 
 export * as adapter from './adapter'
 export * as modules from './modules'
-export * as utils from './utils'
+
+import { safe, safeRead, safeWrite } from './utils'
+export { safe, safeRead, safeWrite }
